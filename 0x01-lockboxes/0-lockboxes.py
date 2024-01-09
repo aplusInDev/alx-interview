e8923a2ebf45fac072ff5354668b8f5642e27adf
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-01-09 23:56:54 +0100

    all done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists. Each index contains a list of integers.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not isinstance(boxes, list):
+        return False
+
+    if len(boxes) == 0:
+        return False
+
+    if len(boxes) == 1:
+        return True
+
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                print(f"key: {key}, box: {box}")
+                keys.append(box)
+
+    if len(keys) == len(boxes):
+        return True
+    else:
+        return False
