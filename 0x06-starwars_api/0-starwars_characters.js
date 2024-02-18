80ab82b6b141115c21a5fe8c9571ba856143703c
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-02-18 15:59:57 +0100

    fixing semistandard by adding error to reject property

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,16 +7,17 @@ request(url, async (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const chars = JSON.parse(body).characters;
     for (const character of chars) {
-      const p = new Promise((resolve, reject) => {
+      const requestPromise = new Promise((resolve, reject) => {
         request(character, (error, response, body) => {
           if (!error && response.statusCode === 200) {
             resolve(JSON.parse(body).name);
-          } else {
-            reject('error');
+          }
+          if (error) {
+            reject(error);
           }
         });
       });
-      console.log(await p);
+      console.log(await requestPromise);
     }
   }
 });
